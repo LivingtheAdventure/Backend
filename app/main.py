2204a2fd3b45c8339a6b6f21d370f3ad009bc0eb
rushikeshwayal <rushikeshwayal6@gmail.com>
2025-10-20 22:42:06 +0530

    updating CORES

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 # ✅ Add CORS Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],  # React/Vite dev server
+    allow_origins=["*"],  # React/Vite dev server
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods
     allow_headers=["*"],  # Allow all headers
