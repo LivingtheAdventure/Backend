d7fde9caf25e5e0eadf05b1fb6a84e609663de90
rushikeshwayal <rushikeshwayal6@gmail.com>
2025-12-31 14:21:40 +0530

    updating cores origin

--- app/main.py
+++ app/main.py
@@ -17,7 +17,9 @@ app.add_middleware(
    allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://frontend-three-mu-66.vercel.app" # <-- Your new frontend URL
+        "https://frontend-three-mu-66.vercel.app", # <-- Your new frontend URL
+        "https://frontend-2-0-ebon.vercel.app"
+
     ],
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods
