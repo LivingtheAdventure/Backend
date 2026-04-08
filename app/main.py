8070a0707a65bbbfdc205a15a582eb9e744bda1f
rushikeshwayal <rushikeshwayal6@gmail.com>
2026-04-08 21:19:40 +0530

    cores

--- app/main.py
+++ app/main.py
@@ -16,7 +16,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://frontend-2-0-ebon.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods
     allow_headers=["*"],  # Allow all headers
