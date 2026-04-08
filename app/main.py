f0ed86c8f6aa6f06cf4514d9e63de64c5c36c27b
rushikeshwayal <rushikeshwayal6@gmail.com>
2026-04-08 21:01:48 +0530

    cores

--- app/main.py
+++ app/main.py
@@ -16,12 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://frontend-three-mu-66.vercel.app",  # <-- Your new frontend URL
-        "https://frontend-2-0-ebon.vercel.app",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods
     allow_headers=["*"],  # Allow all headers
