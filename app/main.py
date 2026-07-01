af7fd2e44c84ffe3e3aeec7301058d78fe432369
rushikeshwayal <rushikeshwayal6@gmail.com>
2026-07-01 19:39:04 +0530

    adding domain url

--- app/main.py
+++ app/main.py
@@ -21,6 +21,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:5173",
         "https://frontend-2-0-ebon.vercel.app",
+        "https://livingtheadventure.in"
     ],
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods
